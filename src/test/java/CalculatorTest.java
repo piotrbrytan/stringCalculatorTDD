cda2585ed1c8e80244135be3f3c7a28072677e04
Piotr Brytan <brytan.piotr@gmail.com>
2020-09-13 10:16:36 +0200

    Step 2: support for multiple numbers

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -15,4 +15,10 @@ class CalculatorTest {
     void add_twoNumbersCommaSeparated_ret4() {
         assertEquals(4, calc.add("2,2"));
     }
+
+    @Test
+    void add_multipleNumbersCommaSeparated_ret100() {
+        assertEquals(100, calc.add("2,2,26,35,5,12,18"));
+    }
+
 }
